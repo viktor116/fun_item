0fcdf8f66185cf2808e3ddc217ea326836a273da
soybean <984149293@qq.com>
2024-10-21 18:01:43 +0800

    update 10/21 update sound unfinished

--- src/main/java/com/soybeani/items/item/PhoneItem.java
+++ src/main/java/com/soybeani/items/item/PhoneItem.java
@@ -1,5 +1,6 @@
 package com.soybeani.items.item;
 
+import com.soybeani.sound.SoundRegister;
 import com.soybeani.utils.CommonUtils;
 import net.minecraft.component.DataComponentTypes;
 import net.minecraft.component.type.FoodComponent;
@@ -7,6 +8,9 @@ import net.minecraft.entity.player.HungerManager;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.text.Text;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
@@ -28,12 +32,24 @@ public class PhoneItem extends Item {
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
         Random random = CommonUtils.getRandom();
         int range = random.nextInt(3);
-        Text text = switch (range) {
-            case 0 -> Text.of("Hello");
-            case 1 -> Text.of("Thank you");
-            case 2 -> Text.of("Bye");
-            default -> Text.of("NO");
-        };
+        Text text = null;
+        switch (range) {
+            case 0:
+                text = Text.of("Are You OK");
+                world.playSound(null, user.getX(), user.getY(), user.getZ(), SoundRegister.ARE_YOU_OK_EVENT, SoundCategory.BLOCKS, 1.0f, 1.0f);
+                break;
+            case 1:
+                text = Text.of("Thank you");
+                world.playSound(null, user.getX(), user.getY(), user.getZ(), SoundRegister.ARE_YOU_OK_EVENT, SoundCategory.BLOCKS, 1.0f, 1.0f);
+                break;
+            case 2:
+                text = Text.of("Bye");
+                world.playSound(null, user.getX(), user.getY(), user.getZ(), SoundRegister.ARE_YOU_OK_EVENT, SoundCategory.BLOCKS, 1.0f, 1.0f);
+                break;
+            default:
+                text = Text.of("NO");
+                world.playSound(null, user.getX(), user.getY(), user.getZ(), SoundRegister.ARE_YOU_OK_EVENT, SoundCategory.BLOCKS, 1.0f, 1.0f);
+        }
         user.sendMessage(text, true);
         HungerManager hungerManager = user.getHungerManager();
         if (hungerManager.isNotFull()) {
--- src/main/java/com/soybeani/sound/SoundRegister.java
+++ src/main/java/com/soybeani/sound/SoundRegister.java
@@ -1,13 +1,27 @@
 package com.soybeani.sound;
 
+import com.soybeani.config.InitValue;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.util.Identifier;
+
 /**
  * @author soybean
  * @date 2024/10/21 17:00
  * @description
  */
 public class SoundRegister {
+    public static final SoundEvent ARE_YOU_OK_EVENT = register("are_you_ok");
 
     public static void initialize(){
 
+    }
+
+    public static SoundEvent register(String id){
+        Identifier soundID = Identifier.of(InitValue.MOD_ID, id);
+        SoundEvent soundEvent = SoundEvent.of(soundID);
+        return Registry.register(Registries.SOUND_EVENT, soundID, soundEvent);
     }
 }
